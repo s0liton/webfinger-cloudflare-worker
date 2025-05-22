c7cd88ab7cbdc6ba55c18239f026e559f2c031c5
Pedro Santos <atruepretense@gmail.com>
2025-05-22 02:23:13 -0400

    Update webfinger.js

--- webfinger.js
+++ webfinger.js
@@ -3,7 +3,7 @@
 export default {
   async fetch(request, env, ctx) {
     const email = "pedro@solitonradar.com"
-    const jsonData = '{"subject":"acct:' + email + '","aliases":["https://' + 'auth.solitonradar.com' + '/@' + pedro + '","https://' + 'auth.solitonradar.com' + '/users/' + 'pedro' + '"],"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://' + 'auth.soltionradar.com' + '/@' + 'pedro' + '"},{"rel":"self","type":"application/activity+json","href":"https://' + 'auth.solitonradar.com' + '/users/' + 'pedro' + '"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://' + 'pedro' + '/authorize_interaction?uri={uri}"}]}';
+    const jsonData = '{"subject":"acct:' + email + '","aliases":["https://' + 'auth.solitonradar.com' + '/@' + 'pedro' + '","https://' + 'auth.solitonradar.com' + '/users/' + 'pedro' + '"],"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://' + 'auth.soltionradar.com' + '/@' + 'pedro' + '"},{"rel":"self","type":"application/activity+json","href":"https://' + 'auth.solitonradar.com' + '/users/' + 'pedro' + '"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://' + 'pedro' + '/authorize_interaction?uri={uri}"}]}';
     return new Response(jsonData, {
       headers: {
         'content-type': 'application/json;charset=UTF-8',
