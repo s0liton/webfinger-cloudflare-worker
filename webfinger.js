24ed6bbdf422ae508e58be7d63ad350803e75e98
Pedro Santos <atruepretense@gmail.com>
2025-05-22 02:44:46 -0400

    Update webfinger.js

--- webfinger.js
+++ webfinger.js
@@ -7,7 +7,7 @@ export default {
       {
         "subject":"acct:" + email,
           "links":[{"rel":"http://openid.net/specs/connect/1.0/issuer", "href":"https://auth.solitonradar.com"}]};
-    jsonDataString = jsonData.toString();
+    let jsonDataString = jsonData.toString();
     return new Response(jsonDataString, {
       headers: {
         'content-type': 'application/json;charset=UTF-8',
